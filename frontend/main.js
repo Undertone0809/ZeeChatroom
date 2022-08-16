dbf36f6ef78a78c66cfec2f9a36b0d57391f6d45
Zeeland <287017217@qq.com>
2022-08-16 23:07:34 +0800

    Create main.js

--- frontend/main.js
+++ frontend/main.js
@@ -3,7 +3,6 @@ import App from './App'
 import io from './components/weapp.socket.io.js';
 
 Vue.config.productionTip = false
-// Vue.prototype.socket = io('http://101.33.214.83:8082')
 Vue.prototype.socket = io('http://localhost:8082')
 
 App.mpType = 'app'
